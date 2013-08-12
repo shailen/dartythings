1f6e97f77271a8c5a535b8d0b0be6e44ba7d5bfe
Shailen Tuli <shailen.tuli@gmail.com>
2013-08-12 16:33:11 -0700

    Removed all the old Polymer examples

--- polymer/build.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-import 'package:polymer/component_build.dart';
-
-main() {
-  build(new Options().arguments, ['web/counter_with_component/index.html',
-                                  'web/counter/index.html',
-                                  'web/bind_to_text/index.html',
-                                  'web/bind_to_text_with_component/index.html']);
-}
