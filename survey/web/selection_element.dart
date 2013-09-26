ba655f1c683d00452e359fa3d5325f722ffe3c1d
Shailen Tuli <shailen.tuli@gmail.com>
2013-09-26 08:46:44 -0700

    Added some styles for the selection element

--- survey/web/selection_element.dart
+++ survey/web/selection_element.dart
@@ -4,6 +4,4 @@ import 'package:survey/models.dart';
 @CustomTag('selection-element')
 class SelectionElement extends PolymerElement with ObservableMixin {
   bool get applyAuthorStyles => true;
-  @observable List<Option> options;
-  @observable bool multiple = true;
 }
