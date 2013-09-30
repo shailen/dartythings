376d74ff4f13e7e0c234c5d4c6d4bf8893e07d1a
Shailen Tuli <shailen.tuli@gmail.com>
2013-09-29 17:28:11 -0700

    Major redo. Game works

--- anagrams/web/element.dart
+++ anagrams/web/element.dart
@@ -1,79 +1,46 @@
 import 'dart:html';
 import 'package:polymer/polymer.dart';
-
-class Char {
-  int position;
-  String value;
-  Char(this.position, this.value);
-  String toString() => '[$position, $value]';
-}
+import 'dart:async';
 
 @CustomTag("word-element")
 class WordElement extends PolymerElement with ObservableMixin {
-  // bool get applyAuthorStyles => true;
-  @observable List<Char> charsList;
+  bool get applyAuthorStyles => true;
 
-  // Get rid of all these lists.  Use charsList as the single canonical list.
-  // Use getters.
-  @observable List<Char> chars1;
-  @observable List<Char> chars2;
-  @observable List<List<Char>> lists;
+  @observable List<String> charsList = toObservable(
+      ['L', 'E', 'A', 'S', 'T', '']);
 
-  @observable int score = 0;
-  List<String> possibleWords = [];
+  List<String> possibleWords = toObservable([
+      'LEAST', 'SETAL', 'SLATE', 'STALE', 'STEAL', 'STELA', 'TAELS', 'TALES',
+      'TEALS', 'TESLA', 'AE', 'AL', 'AS', 'AT', 'EL', 'ES', 'ET', 'LA', 'TA',
+      'ALE', 'ALS', 'ALT', 'ATE', 'EAT', 'ELS', 'ETA', 'LAS', 'LAT', 'LEA',
+      'LES', 'LET', 'SAE', 'SAL', 'SAT', 'SEA', 'SEL', 'SET', 'TAE', 'TAS',
+      'TEA', 'TEL', 'ALES', 'ALTS', 'ATES', 'EAST', 'EATS', 'ETAS', 'LASE',
+      'LAST', 'LATE', 'LATS', 'LEAS', 'LEST', 'LETS', 'SALE', 'SALT', 'SATE',
+      'SEAL', 'SEAT', 'SETA', 'SLAT', 'TAEL', 'TALE', 'TEAL', 'TEAS', 'TELA',
+      'TELS']);
+
   List<String> formedWords = [];
 
+  @observable int score = 0;
   int sourceElementIndex;
   int targetElementIndex;
   @observable String word;
 
   String upCase(String str) => str.toUpperCase();
-
-  get firstSubList => (list) => [list.getRange(0, word.length).toList()];
-  get secondSubList => (list) => [list.getRange(word.length, list.length).toList()];
-
   created() {
     super.created();
-    possibleWords = toObservable(['least', 'setal', 'slate', 'stale', 'steal', 'stela', 'taels', 'tales',
-                     'teals', 'tesla', 'ae', 'al', 'as', 'at', 'el', 'es', 'et', 'la', 'ta', 'ale',
-                     'als', 'alt', 'ate', 'eat', 'els', 'eta', 'las', 'lat', 'lea', 'les', 'let',
-                     'sae', 'sal', 'sat', 'sea', 'sel', 'set', 'tae', 'tas', 'tea', 'tel', 'ales',
-                     'alts', 'ates', 'east', 'eats', 'etas', 'lase', 'last', 'late', 'lats',
-                     'leas', 'lest', 'lets', 'sale', 'salt', 'sate', 'seal', 'seat', 'seta',
-                     'slat', 'tael', 'tale', 'teal', 'teas', 'tela', 'tels']);
-
-    word = possibleWords.first;
-
-    charsList = toObservable(new List(word.length * 2));
-
-    // TODO: sprinkle words over both rows.
-    for (var i = 0; i < word.length; i++) {
-      charsList[i] = new Char(i, word[i]);
-      var blank = i + word.length;
-      charsList[blank] = new Char(blank, ' ');
-    }
-
-    chars1 = toObservable(new List(word.length));
-    chars2 = toObservable(new List(word.length));
-    _filterChars();
-    lists = toObservable([chars1, chars2]);
   }
-
-  _filterChars() {
-    for (var i = 0; i < word.length; i++) {
-      chars1[i] = charsList[i];
-      chars2[i] = charsList[i + word.length];
-    }
-  }
-
 
   void dragStartHandler(Event e, detail, sender) {
-    e.target.style.opacity = '.25';
-    sourceElementIndex = int.parse(e.target.attributes['position']);
+    print('in dragStartHandler');
+    sourceElementIndex = charsList.indexOf(sender.text.trim());
+    // sender.style.opacity = '.25';
   }
 
   void dragEnterHandler(e) {
-    e.target.classes.add('over');
+    print('in dragEnterHandler');
+    // TODO: fix the over class CSS
+    // e.target.classes.add('over');
   }
 
   void dragOverHandler(e) {
@@ -85,34 +52,51 @@ class WordElement extends PolymerElement with ObservableMixin {
     e.target.classes.remove('over');
   }
 
-  void dropHandler(Event e) {
+  void dropHandler(Event e, detail, sender) {
+    print('in dropHandler');
     e.preventDefault();
-    targetElementIndex = int.parse(e.target.attributes['position']);
-    var temp = charsList[sourceElementIndex].value;
-    charsList[sourceElementIndex].value = charsList[targetElementIndex].value;
-    charsList[targetElementIndex].value = temp;
-    _filterChars();
+    targetElementIndex = charsList.indexOf(sender.text.trim());
+
+    var temp = charsList[sourceElementIndex];
+    charsList[sourceElementIndex] = charsList[targetElementIndex];
+    charsList[targetElementIndex] = temp;
+    print(charsList);
+    new Timer(new Duration(milliseconds: 125), () {
+      //////////////////////////
+      var wordIndices = [];
+      var wordChars = [];
+
+      var root = getShadowRoot('word-element');
+      var charDivs = root.queryAll('.char');
+
+      for (var i = 0; i < charsList.length; i++) {
+        if (charsList[i].isNotEmpty) {
+          wordIndices.add(i);
+          wordChars.add(charsList[i]);
+        }
+
+        // XXX: clunky
+        if (charsList[i].isEmpty || i == charsList.length - 1) {
+          var word = wordChars.join('');
+          if (possibleWords.contains(word) && !formedWords.contains(word)) {
+            formedWords.add(word);
+            score += word.length;
+            for (var i in wordIndices) {
+              print(charDivs[i]);
+              charDivs[i].classes.add('yellow');
+            }
+          }
+          wordIndices = [];
+          wordChars = [];
+        }
+      }
+      ///////////////////////////
+   });
   }
 
   void dragEndHandler(Event e, detail, sender) {
+    print('in dragEndHandler');
     e.preventDefault();
     e.stopPropagation();
-    e.target.style.opacity = '1.0';
-    var words = [];
-    [chars1, chars2].forEach((chars) {
-      var tokens = chars.map((char) => char.value).toList();
-      var fragments = tokens.join('').split(' ').where(
-          (token) => token.trim().isNotEmpty).toList();
-      words.addAll(fragments);
-    });
-
-    for (var w in words) {
-      if (possibleWords.contains(w)) {
-        if (!formedWords.contains(w)) {
-          formedWords.add(w);
-          score += w.length;
-        }
-      }
-    }
   }
 }
